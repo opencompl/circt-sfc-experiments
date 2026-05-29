8ba2eacd66d15513743a1fe38a99efa3dab46533
Amelia Dobis <amelia.dobis@princeton.edu>
2026-05-29 09:48:50 -0400

    added constraint file generation (name isn't generic)

--- utils/gen_config.py
+++ utils/gen_config.py
@@ -28,18 +28,51 @@ export SWAP_ARITH_OPERATORS = 1
 export OPENROAD_HIERARCHICAL = 1
 """
 
+constraint_base = lambda name: f"""
+current_design {name}
+
+set clk_name core_clock
+set clk_port_name clk_i
+set clk_period 10.0
+set clk_io_pct 0.2
+
+set clk_port [get_ports $clk_port_name]
+
+create_clock -name $clk_name -period $clk_period $clk_port
+
+set non_clock_inputs [all_inputs -no_clocks]
+
+set_input_delay [expr $clk_period * $clk_io_pct] -clock $clk_name $non_clock_inputs
+set_output_delay [expr $clk_period * $clk_io_pct] -clock $clk_name [all_outputs]
+"""
+
 if __name__ == "__main__":
     assert len(sys.argv) > 2, "Usage: gen_config.py <design_name> <path_to_output_directory>"
+    
+    # Check that the given path is valid
+    if not os.path.isdir(sys.argv[2]):
+        print(f"{sys.argv[2]} is not a valid directory!")
+        exit(1)
 
-    out = sys.argv[2] + "/config.mk"
+    # Output paths
+    out_config = sys.argv[2] + "/config.mk"
+    out_const = sys.argv[2] + "/constraint.sdc"
 
-    # Check if a config already exists at that location
-    if os.path.isfile(out):
+    # Check if one of the files already exists at that location
+    if os.path.isfile(out_config):
         print("config file already exists at that location")
         exit(1)
 
-    # create the config
-    with open(out, 'w') as conf:
-        conf.write(config_base(sys.argv[1]))
+    if os.path.isfile(out_const):
+        print("constraint file already exists at that location")
+        exit(1)
 
-    print(f"Config created at {out}")
+    # create the files
+    with open(out_config, 'w') as conf:
+        conf.write(config_base(sys.argv[1]))
+    
+    with open(out_const, 'w') as constr:
+        constr.write(constraint_base(sys.argv[1]))
+
+    print(f"Files created at:\n\t{out_config}\n\t{out_const}")
+
